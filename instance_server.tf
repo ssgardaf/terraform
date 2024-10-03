c321499b28905beb7975fb8ed47f99d686b60eac
Jenson <ssgardaf@gmail.com>
2024-10-03 18:07:25 +0900

    EC2 셋팅 업데이트

--- instance_server.tf
+++ instance_server.tf
@@ -1,7 +1,32 @@
+resource "aws_security_group" "websocket_sg" {
+  name        = "websocket-sg"
+  description = "Allow inbound traffic for WebSocket"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
 resource "aws_instance" "websocket_server" {
-  ami           = "ami-0a01fe64fe4ac6319"  # Ubuntu AMI ID
-  instance_type = "t3.micro"
-  key_name      = "your-key-name"
+  ami           = "ami-01abb191f665c021f"  # Ubuntu AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnets[0].id
+  vpc_security_group_ids = [aws_security_group.websocket_sg.id]
+
 
   user_data = <<-EOF
               #!/bin/bash
@@ -56,20 +81,3 @@ resource "aws_instance" "websocket_server" {
   }
 }
 
-resource "aws_security_group" "websocket_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
